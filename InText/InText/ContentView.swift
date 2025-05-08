63f567c39c2107138718923059b5ff1c0271889a
kerikgg <kirill.abramov.24@mail.ru>
2025-05-08 15:36:52 +0300

    Init project

---
+++ InText/InText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InText
+//
+//  Created by kerik on 08.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InText/InText/InTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  InTextApp.swift
+//  InText
+//
+//  Created by kerik on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct InTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
